b5d525e6d9ad96caafbb044d7e2c7560c41e672c
jongwooo <han980817@gmail.com>
2022-10-27 03:24:48 +0900

    feat: Add validate logic

--- calculator/views.py
+++ calculator/views.py
@@ -9,7 +9,7 @@ def home(request):
 def result(request):
     raw_data = request.GET['data']
     raw_polynomial_code = request.GET['polynomial-code']
-    raw_surplus_data = request.GET['surplus-data']
+    raw_remainder = request.GET['remainder']
 
     binary_pattern = re.compile('^1[01]*$')
     surplus_pattern = re.compile('^[01]+$')
@@ -18,20 +18,22 @@ def result(request):
         data = [int(d) for d in raw_data]
         polynomial_code = [int(c) for c in raw_polynomial_code]
 
-        if raw_surplus_data == "":
-            surplus_data = [0 for _ in range(0, len(polynomial_code) - 1)]
-        elif bool(surplus_pattern.match(raw_surplus_data)) and len(raw_surplus_data) == len(polynomial_code) - 1:
-            surplus_data = [int(s) for s in raw_surplus_data]
-        else:
-            return render(request, 'error.html')
+        if raw_remainder == "":
+            remainder = [0 for _ in range(0, len(polynomial_code) - 1)]
+            checksum = get_checksum(data, polynomial_code, remainder)
+            return render(request, 'result.html', {'checksum': ''.join(str(c) for c in checksum)})
 
-        return render(request, 'result.html', {'checksum': get_checksum(data, polynomial_code, surplus_data)})
+        elif bool(surplus_pattern.match(raw_remainder)) and len(raw_remainder) == len(polynomial_code) - 1:
+            remainder = [int(s) for s in raw_remainder]
+            checksum = get_checksum(data, polynomial_code, remainder)
+            is_valid = checksum == [0 for _ in range(0, len(polynomial_code) - 1)]
+            return render(request, 'validate.html', {'is_valid': is_valid})
 
     return render(request, 'error.html')
 
 
-def get_checksum(data, polynomial_code, surplus_data):
-    combined_data = data + surplus_data
+def get_checksum(data, polynomial_code, remainder):
+    combined_data = data + remainder
     combined_data_size, polynomial_code_size = len(combined_data), len(polynomial_code)
     calculation_count = combined_data_size - polynomial_code_size + 1
 
@@ -45,7 +47,7 @@ def get_checksum(data, polynomial_code, surplus_data):
         if quotient_digit != calculation_count - 1:
             temp.append(combined_data[polynomial_code_size + quotient_digit])
 
-    return ''.join(str(t) for t in temp)
+    return temp
 
 
 def get_xor(list1, list2):
